3a2d1bc006e71e2e0fbc948a194b81ce0cb00f92
charles <charles@charless-MacBook-Pro.local>
2017-03-29 22:58:01 -0500

    eval parse

--- public/scripts/t01.js
+++ public/scripts/t01.js
@@ -1,29 +1,47 @@
 var app = angular.module("app",[]);
 
-app.controller("msg",['$scope','$rootScope',function($scope,$rootScope){
+app.controller("sample",['$scope','$parse',function($scope,$parse){
 
-$scope.data=[
-  {v:17,r:0},
-  {v:24,r:0},
-  {v:32,r:0},
-  {v:48,r:0},
-]
+  $scope.a=10;
+  $scope.b=20;
+  $scope.emp={
+    name:"charles",
+    address:{
+        city:"austin"
+    }
+  };
+
+   $scope.demoEval= function(){
+  //
+  // //   alert( "Result: "+($scope.a + $scope.b));
+  // //   alert("Result: "+ $scope.$eval("a + b"));
+  // //  alert( $scope.$eval("emp.address.city"));
+  // //   var r= $scope.$eval("a * b * 3 * c",{
+  // //     c: 5
+  // //   });
+  // //   alert(r);
+  //
+  //   var r =$scope.$eval(function(scope,locals){
+  //     return scope.a * scope.b * locals.c;
+  //   },{c:2});
+  //   alert(r);
+   }
+
+   $scope.demoParse = function (){
+    //  var f= $parse("a * b");  //return a function
+    //  var r=f($scope);
+    //  alert(r);
+
+    //  alert($parse("a * b")($scope));
+    alert($parse("a * b")({
+      a:2,
+      b:3
+    }));
+   }
+
+   $scope.demoInterpolate= function(){
+
+     
+   }
 
 }]);
-
-app.directive('message',function(){
-  return{
-    templateUrl:'view/info-msg.htm',
-    compile: function(){
-      return function(scope,iElement,iAttribute,controller){
-        iElement.find("#btnSum").on("click",scope.btnClick);
-      }
-    },
-    controller: function($scope, $element, $attrs){
-      $scope.btnClick=function(){
-        $scope.o.r=$scope.o.v * $scope.o.v;
-        $scope.$digest();
-      }
-    }
-  }
-});
