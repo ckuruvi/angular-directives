c7e66bfa9a51a9d7bf2853a443b931edd6a0a477
charles <charles@charless-MacBook-Pro.local>
2017-03-29 19:33:57 -0500

    digest cycle example

--- public/scripts/t01.js
+++ public/scripts/t01.js
@@ -1,55 +1,37 @@
 var app = angular.module("app",[]);
 
-app.controller("msg",['$scope',function($scope){
+app.controller("emp",['$scope','$rootScope',function($scope,$rootScope){
 
 $scope.a=1;
 $scope.b=2;
 $scope.c=4;
 
-// $scope.updateC=function(){
-//   $scope.c=$scope.a * 4;
-// }
-//
-// $scope.$watch('a',function(newVal,oldVal){
-//
-//   if(newVal != oldVal){
-//     $scope.b=$scope.a * 2;
-//   }
-// })
-//
-// $scope.$watch('b',function(newVal,oldVal){
-//
-//   if(newVal != oldVal){
-//     $scope.c=$scope.b * 2;
-//   }
-// })
-//
-//
-// $scope.$watch('c',function(newVal,oldVal){
-//
-//   if(newVal != oldVal){
-//     console.log("Updated C to "+ newVal);
-//   }
-// })
-
-// $scope.$watchGroup(['a','b'],function(newVal,oldVal){
-//
-//   if(newVal != oldVal){
-//     $scope.c=$scope.b * $scope.a;
-//   }
-// })
-
-
-$scope.o = {
-  a:1,
-  b:2,
-  c:4
-};
-
-$scope.$watch('o',function(newVal,oldVal){
-  console.log("inside watch");
+$scope.$watch('a',function(newVal,oldVal){
   if(newVal != oldVal){
-    $scope.o.c=$scope.o.a * $scope.o.b;
+    console.log("a modified to"+newVal);
+
   }
-},true);
+});
+
+$scope.$watch('b',function(newVal,oldVal){
+  if(newVal != oldVal){
+    console.log("b modified to"+newVal);
+
+  }
+
+});
+
+$scope.$watch('c',function(newVal,oldVal){
+  if(newVal != oldVal){
+    console.log("c modified to"+newVal);
+    if($scope.c >50){
+      $scope.a=1000;
+    }
+  }
+});
+
+$rootScope.$watch(function(){
+  console.log("digest cycle started");
+});
+
 }]);
